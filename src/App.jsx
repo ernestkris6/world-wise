79af50610e6d64655c071beed7d8fbcb21d48055
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-26 14:09:44 +0100

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import Pricing from "./Pages/Pricing"
 import Product from "./Pages/Product"
+import PageNotFound from "./Pages/PageNotFound"
 
 export default function App() {
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />}/>
         <Route path="pricing" element={<Pricing />}/>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
     </div>
