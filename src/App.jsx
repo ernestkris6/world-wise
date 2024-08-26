a0f849bfe2bb145175a05d94348495f592655440
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-26 14:05:54 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import Home from "./Components/Pages/Home"
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Pricing from "./Pages/Pricing"
+import Product from "./Pages/Product"
 
 export default function App() {
 
     
   return (
+   
     <div>
-     <Home />
+       <h4>Hello Routes!</h4>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />}/>
+        <Route path="pricing" element={<Pricing />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
+    
+    
   )
 }
