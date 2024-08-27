bcd05bc84d23671f385aa99f09f8e49c85c151c0
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-27 15:51:47 +0100

    finishing the layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./Pages/Pricing"
 import Product from "./Pages/Product"
 import PageNotFound from "./Pages/PageNotFound"
 import AppLayout from "./Components/AppLayout"
+import Login from "./Pages/Login"
 
 
 
@@ -19,8 +20,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<Product />}/>
         <Route path="/pricing" element={<Pricing />}/>
-        <Route path="*" element={<PageNotFound />} />
         <Route path="/app" element={<AppLayout/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
     </div>
