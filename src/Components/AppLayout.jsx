ef783bbe6811cd79fcb429ee429616c8db6c6374
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-30 23:57:27 +0100

    map

--- src/Components/AppLayout.jsx
+++ src/Components/AppLayout.jsx
@@ -1,10 +1,12 @@
 // import AppNav from '../Components/AppNav'
 import styles from "./AppLayout.module.css"
+import SideBar from "./SideBar"
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
      <h1>App Section</h1>
+     <SideBar />
     
     </div>
   )
