fe9ed0643627bbec1c84a2f0e18af02e77910980
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-02 07:51:52 +0100

    sidebar

--- src/Components/AppLayout.jsx
+++ src/Components/AppLayout.jsx
@@ -6,7 +6,6 @@ import SideBar from "./SideBar"
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-     <h1>App Section</h1>
      <SideBar />
      <Map />
     
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,12 +1,19 @@
+import AppNav from "../Components/AppNav"
+import Logo from "../Components/Logo"
 
+
+import styles from "../Components/SideBar.module.css";
 
 export default function SideBar() {
   return (
-    <div>
-      SideBar
-      <footer>
+    <div className={styles.sideBar}>
+     <Logo />
+     <AppNav />
+     <p>List of Cities</p>
+     <footer className={styles.footer}>
         &copy; 2024
       </footer>
+      
     </div>
     
   )
