200dc6c1238ac885c22ebd5667252a3edfc4dc38
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-04 19:36:55 +0100

    position lat and lng

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,8 +1,19 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 
 export default function Map() {
+
+  const [searchParams, setSearchParams]  = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng")
+
   return (
-    <div className={styles.mapContainer}>Map</div>
+    <div className={styles.mapContainer}>
+        <h1>Map</h1>
+        <h3>{lat}</h3>
+        <h3>{lng}</h3>
+    </div>
   )
 }
