82dfc317744a2dcf35f17ff08cad1497d050a1b0
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-24 23:52:31 +0100

    export useGeolocate

--- src/Hooks/useGeoLocate.js
+++ src/Hooks/useGeoLocate.js
@@ -9,10 +9,19 @@ function getPosition(){
         setError("Your browser does not support geolocation")
     )
     setIsLoading(true);
-    navigator.geolocation.getCurrentPosition((pos)=> {
+    navigator.geolocation.getCurrentPosition(
+        (pos)=> {
         setPosition({
             lat: pos.coords.latitude,
             lng: pos.coords.longitude
         });
-    })
+        setIsLoading(false);
+    },
+    (error)=> {
+        setError(error.message);
+        setIsLoading(false)
+    }
+)
+
+return {isLoading, position, error, getPosition}
 }
